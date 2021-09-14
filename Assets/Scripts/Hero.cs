71662c1ef502c451be717b321b48a3b109c92417
elseeeey <49165040+elseeeey@users.noreply.github.com>
2021-09-14 22:22:13 +0300

    added base

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,84 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+public class Hero : Entity
 {
     [SerializeField] private float speed = 3f; // скорость движения
     [SerializeField] private int lives = 5; // скорость движения
-    [SerializeField] private float jumpForce = 15f; // сила прыжка
-    private bool isGrounded = false;
-
-    private Rigidbody2D rb;
-    private Animator anim;
+    [SerializeField] private float jumpforce = 15f; // сила прыжка
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
     private SpriteRenderer sprite;
+
+    public static Hero Instance { get; set; }
 
     private States State
     {
-        get { return (States)anim.GetInteger("state"); }
-        set { anim.SetInteger("state", (int)value); }
+        get { return (States)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
     }
 
+    private bool isGrounded = false;
+   
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        Instance = this;
+
     }
 
-    //private void FixedUpdate()
-    //{
-    //    CheckGround();
-    //}
+    private void FixedUpdate()
+    {
+        Checkground();
+    }
 
     private void Update()
     {
         if (isGrounded) State = States.idle;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-        if (Input.GetButton("Horizontal"))
-            Run();
-        ////if (Input.GetButton("Jump") /*&& isGrounded*/)
-        ////{
-        ////    Jump();
-        ////    isGrounded = false;
-        ////}
-        //print(isGrounded);
-
-
-        //if (isGrounded && Input.GetButtonDown("Jump"))
-        //{
-        //    print("123");
-        //    Debug.Log("Jump");
-        //    Jump();
-        //}
-
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButton("Jump")) Jump();
 
     }
 
     private void Run()
     {
         if (isGrounded) State = States.run;
-        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-        sprite.flipX = dir.x < 0.0f;
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x < 0.0f;
     }
 
     private void Jump()
     {
-        rb.velocity = Vector2.up * jumpForce;
+        rigidbody.AddForce(transform.up * jumpforce, ForceMode2D.Impulse);
+
+        
     }
 
-    //private void CheckGround()
-    //{
-    //    Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
-    //    isGrounded = collider.Length > 1;
+    private void Checkground()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3f);
 
-    //    if (isGrounded) State = States.jump;
-    //}
+        isGrounded = colliders.Length > 1;
+
+        if (!isGrounded) State = States.jump;
+    }
+
+    public override void GetDamage()
+    {
+        lives -= 1;
+        Debug.Log(lives);
+    }
+ 
 
 }
+
     public enum States
     {
         idle,
